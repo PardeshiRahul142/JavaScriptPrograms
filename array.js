a087e3c35365b146bc474dfc87329044f5c6460a
RahulPardeshi <rahulpardeshi949@gmail.com>
2026-01-04 12:28:34 +0530

    this is array

--- array.js
+++ array.js
@@ -1,8 +1,16 @@
-let a=[10,20,30,40,50];
+/*let a=[10,20,30,40,50];
 console.log(a);
 
 console.log("Using for loop can fetch data");
 for(var i=0;i<a.length;i++)
 {
     console.log(a[i]);
+}*/
+
+let a=new Array(10,20,30,40,50);
+console.log(a);
+console.log("Featching data from array");
+for(var i=0;i<a.length;i++)
+{
+   console.log(a[i]);
 }

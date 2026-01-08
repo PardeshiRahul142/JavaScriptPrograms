2da4b660eaf8d202141dd1d619393539a059d906
RahulPardeshi <rahulpardeshi949@gmail.com>
2026-01-08 11:15:47 +0530

    this is lastIndexOf()

---
+++ lastIndexOf.js
@@ -0,0 +1,6 @@
+//laseIndexOf(); this method use for check element present in array if present return index and its return last occurance element;
+
+let a=[19,29,13,32,15,32,20,30,32];
+
+let ind=a.lastIndexOf(32);
+console.log(ind);
